e3f16c32c35451cac1bb406c8ee5a1d3bcef9dee
joeri2001 <joeribrinks68@gmail.com>
2024-11-21 11:51:04 +0100

    add navigation component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,10 @@
+import NavBar from "@/components/navigation/navbar"
+
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <p>Hello, World!</p>
-    </div>  
+    </div>
   );
 }
