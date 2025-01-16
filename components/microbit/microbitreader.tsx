9bb2f6f647c8b362da086574e9498b2ff7630b9e
joeri2001 <joeribrinks68@gmail.com>
2025-01-16 11:29:19 +0100

    changed reader

--- components/microbit/microbitreader.tsx
+++ components/microbit/microbitreader.tsx
@@ -5,7 +5,7 @@ import { useMoistureSensor } from "./sensors/useTestSensor";
 
 export default function MicrobitReader() {
   const { isConnected, error, port, connectToMicrobit } = useSerialConnection();
-  const moisture = useMoistureSensor(port);
+  const { moisture } = useMoistureSensor(port);
 
   return (
     <div className="bg-white shadow-md rounded-lg p-6 max-w-sm mx-auto">
